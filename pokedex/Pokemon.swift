dbe4717013beaaf2be44fd01b316bddb59527037
Jonny B <jonnyb@Jonnys-MacBook-Pro.local>
2015-12-22 21:39:36 -0700

    updates before starting alamofire

--- pokedex/Pokemon.swift
+++ pokedex/Pokemon.swift
@@ -15,6 +15,13 @@ class Pokemon {
     // Create the variables for the class, these are private variables.
     private var _name: String!
     private var _pokedexId: Int!
+    private var _description: String!
+    private var _type: String!
+    private var _defense: String!
+    private var _height: String!
+    private var _weight: String!
+    private var _attack: String!
+    private var _nextEvolutionTxt: String!
     
     // Create the getters for the variables.
     var name: String {
--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -12,11 +12,25 @@ class PokemonDetailVC: UIViewController {
 
     var pokemon: Pokemon!
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var baseAttackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+   
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        nameLbl.text = pokemon.name
+        nameLbl.text = pokemon.name.capitalizedString
         
     }
 
@@ -25,5 +39,8 @@ class PokemonDetailVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func bacBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
+    }
 
 }
